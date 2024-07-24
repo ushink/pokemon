4f4ba7b449b6c82f87498c1c0575f869d9f0c33e
Ulyana <ushinkevich@mail.ru>
2024-07-24 09:19:09 +0400

    mock

---
+++ src/mock.js
@@ -0,0 +1,42 @@
+export const mockPokemonData = [
+  {
+    id: 25,
+    name: "Pikachu",
+    type: "Electric",
+    abilities: ["Static", "Lightning Rod"],
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    type: "Fire",
+    abilities: ["Blaze", "Solar Power"],
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    type: "Water",
+    abilities: ["Rain Dish", "Torrent"],
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    type: "Water",
+    abilities: ["Rain Dish", "Torrent"],
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    type: "Bug/Flying",
+    abilities: ["Compound Eyes", "Gale Wings"],
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+  },
+];
